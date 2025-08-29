f69b445d83254beefc3c955a515b18a928f20669
gitsang-dev <committedlim@gmail.com>
2025-08-30 01:36:31 +0900

    fix: cn 함수 import 추가

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Card, CardContent } from './ui/card'
+import { cn } from '@/lib/utils'
 import { Button } from './ui/button'
 import { ThumbsUp, MessageCircle, MoreVertical, Pencil, Trash2, User, Clock } from 'lucide-react'
 import {
